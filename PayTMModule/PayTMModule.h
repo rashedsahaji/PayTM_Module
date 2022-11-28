fab73ce29114b160143e2ac22e328c105fd9b37c
rashed.sahajee <rashed.sahajee@bigohtech.com>
2022-11-28 11:30:19 +0530

    Initial Commit

---
+++ PayTMModule/PayTMModule.h
@@ -0,0 +1,18 @@
+//
+//  PayTMModule.h
+//  PayTMModule
+//
+//  Created by Rashed Sahajee on 28/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PayTMModule.
+FOUNDATION_EXPORT double PayTMModuleVersionNumber;
+
+//! Project version string for PayTMModule.
+FOUNDATION_EXPORT const unsigned char PayTMModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PayTMModule/PublicHeader.h>
+
+
